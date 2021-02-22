0bbea28397e90265d021efc38e41214210726dcb
Sridhar Lanka <sridharlanka86@gmail.com>
2021-02-22 12:45:56 +0530

    My First commit

---
+++ selfReturn/Controllers/ReturnsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace selfReturn.Controllers
+{
+    [Route("api/returns")]
+    [ApiController]
+    public class ReturnsController : ControllerBase
+    {
+        // GET api/returns
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Hello", "Returns" };
+        }
+    }
+}
